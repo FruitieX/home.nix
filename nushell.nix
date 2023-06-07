d574741db616e134591cbe9a16ba9025f74a228b
Rasmus Lövegren <rasmus@lovegren.fi>
2023-06-07 10:49:04 +0300

    feat: enable vi mode in nushell prompt

--- nushell.nix
+++ nushell.nix
@@ -4,33 +4,33 @@
 {
   enable = true;
 
+  # extraEnv = ''
+  #   zoxide init nushell | save -f ~/.zoxide.nu
+  # '';
+
   extraConfig = ''
+    let carapace_completer = {|spans| 
+      carapace $spans.0 nushell $spans | from json
+    }
+
     let-env config = {
+      show_banner: false,
+      edit_mode: vi,
+
       hooks: {
         pre_prompt: [{ ||
           let direnv = (direnv export json | from json)
           let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
           $direnv | load-env
         }]
-      }
-    }
+      },
 
-    let carapace_completer = {|spans| 
-      carapace $spans.0 nushell $spans | from json
-    }
-
-    let-env config = {
       completions: {
         external: {
           enable: true
           completer: $carapace_completer
         }
       }
-    }
-
-    let-env config = {
-      show_banner: false,
-      edit_mode: vi
     }
 
     alias da = direnv allow
